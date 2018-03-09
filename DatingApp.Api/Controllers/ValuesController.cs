1d8d8eb5c7f2f185b67039698c73196ab2ef6dbc
Ajit <codemasterblackperl@outlook.com>
2018-03-09 10:46:30 +0530

    Add project files.

---
+++ DatingApp.Api/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly DataContext _data;
+
+        public ValuesController(DataContext dataContext)
+        {
+            _data = dataContext;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _data.Value.ToList();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            return Ok();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.Api/Migrations/20180308182043_AddedFirstModelToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DatingApp.Api.Migrations
+{
+    public partial class AddedFirstModelToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Value",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Value", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Value");
+        }
+    }
+}
